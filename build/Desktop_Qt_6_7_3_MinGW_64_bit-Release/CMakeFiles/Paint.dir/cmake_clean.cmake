0fdd2d09968819b9c5b9e24fd4f134bc3d4819e5
Vi Ali <vova4444919@gmail.com>
2025-08-14 12:13:09 +0300

    100

---
+++ build/Desktop_Qt_6_7_3_MinGW_64_bit-Release/CMakeFiles/Paint.dir/cmake_clean.cmake
@@ -0,0 +1,36 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Paint_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/Paint_autogen.dir/ParseCache.txt"
+  "Paint_autogen"
+  "CMakeFiles/Paint.dir/PaintArea.cpp.obj"
+  "CMakeFiles/Paint.dir/PaintArea.cpp.obj.d"
+  "CMakeFiles/Paint.dir/Paint_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/Paint.dir/Paint_autogen/mocs_compilation.cpp.obj.d"
+  "CMakeFiles/Paint.dir/Shapes/BaseShape.cpp.obj"
+  "CMakeFiles/Paint.dir/Shapes/BaseShape.cpp.obj.d"
+  "CMakeFiles/Paint.dir/Shapes/ellipse.cpp.obj"
+  "CMakeFiles/Paint.dir/Shapes/ellipse.cpp.obj.d"
+  "CMakeFiles/Paint.dir/Shapes/rectangle.cpp.obj"
+  "CMakeFiles/Paint.dir/Shapes/rectangle.cpp.obj.d"
+  "CMakeFiles/Paint.dir/Shapes/triangle.cpp.obj"
+  "CMakeFiles/Paint.dir/Shapes/triangle.cpp.obj.d"
+  "CMakeFiles/Paint.dir/connection.cpp.obj"
+  "CMakeFiles/Paint.dir/connection.cpp.obj.d"
+  "CMakeFiles/Paint.dir/main.cpp.obj"
+  "CMakeFiles/Paint.dir/main.cpp.obj.d"
+  "CMakeFiles/Paint.dir/mainwindow.cpp.obj"
+  "CMakeFiles/Paint.dir/mainwindow.cpp.obj.d"
+  "CMakeFiles/Paint.dir/mainwindowSlots.cpp.obj"
+  "CMakeFiles/Paint.dir/mainwindowSlots.cpp.obj.d"
+  "Paint.exe"
+  "Paint.exe.manifest"
+  "Paint.pdb"
+  "Paint_autogen/mocs_compilation.cpp"
+  "Paint_autogen/timestamp"
+  "libPaint.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Paint.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
